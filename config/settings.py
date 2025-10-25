c04bb7f39ba380d724195d1ba6a576ea5c8824c7
epqlffltm <dsfake1225@gmail.com>
2025-10-26 01:26:21 +0900

    bd끝

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "burgers"
+    "burgers",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- config/views.py
+++ config/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from burgers.models import Burger
 
 def main(request):
   return render(request,"main.html")
 
 def burger_list(request):
-  return render(request,"burger_list.html")
+  burgers = Burger.objects.all()
+  print("전체 햄버거 목록: ",burgers)
+  content = {"burgers":burgers}
+  return render(request,"burger_list.html",content)
