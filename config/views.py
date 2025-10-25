8dee88aff6adddf03a383d9637d03c7e6daee622
epqlffltm <dsfake1225@gmail.com>
2025-10-26 04:45:13 +0900

    완성

--- config/views.py
+++ config/views.py
@@ -9,3 +9,12 @@ def burger_list(request):
   print("전체 햄버거 목록: ",burgers)
   content = {"burgers":burgers}
   return render(request,"burger_list.html",content)
+
+def burger_search(request):
+  print(request.GET)
+  keyword = request.GET.get("keyword")
+  burgers = Burger.objects.filter(name__icontains=keyword) if keyword else Burger.objects.none()
+  print(keyword)
+  print("검색된 햄버거 목록:", burgers)
+  context = {"burgers":burgers}
+  return render(request,"burger_search.html",context)
